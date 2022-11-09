5845bf91280792351b4b8fd40bf6eda4d44af274
Eugeny <sgenya72@gmail.com>
2022-11-09 19:21:44 +0300

    filled the class Program

--- Sword/Program.cs
+++ Sword/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Sword
 {
@@ -10,6 +11,50 @@ namespace Sword
     {
         static void Main(string[] args)
         {
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+
+            int diceRoll = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                diceRoll += random.Next(1, 7);
+            }
+            Console.WriteLine("dice roll: " + diceRoll);
+            swordDamage.Roll = diceRoll;
+
+            Console.WriteLine("enter which sword: 0 for no magic/flaming," +
+                " 1 for magic,2 for flaming, 3 for both," +
+                " anything else to quit");
+
+            int whichSword = int.Parse(Console.ReadLine());
+            if (whichSword == 0)
+            {
+                swordDamage.CalculateDamage();
+                Console.WriteLine($"Rolled for {swordDamage.Damage} HP");
+                
+            }
+            else if (whichSword == 1)
+            {
+                swordDamage.SetMagic(true);
+                Console.WriteLine($"Rolled for {swordDamage.Damage} HP");
+            }
+            else if (whichSword == 2)
+            {
+                swordDamage.SetFlaming(true);
+                Console.WriteLine($"Rolled for {swordDamage.Damage} HP");
+            }
+            else if (whichSword == 3)
+            {
+                swordDamage.SetMagic(true);
+                swordDamage.SetFlaming(true);
+                Console.WriteLine($"Rolled for {swordDamage.Damage} HP");
+            }
+            else
+            {
+                Console.WriteLine("please, enter correct number");
+                return;
+            }
+
         }
     }
 }
